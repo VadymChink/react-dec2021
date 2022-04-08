9d388b2b67152f15c39695d6563a904a3eb9d193
VadymChink <vadim-vsnnschuk@ro.ru>
2022-04-08 13:28:09 +0300

    End hw1

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -4,9 +4,9 @@ import './Post.css'
 export const Post = ({post}) => {
     const {id, userId, title, body} = post
     return (
-        <div>
-            <span>Post id: {id}</span>
-            <span>User id: {userId}</span>
+        <div className={'post'}>
+            <span>Post id: {id} </span>
+            <span>User id: {userId} </span>
             <span>Title: {title}</span>
             <div>Body: {body}</div>
         </div>

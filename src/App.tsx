f7fe03bf8e84e321614ddf279b097e4702e49bad
VadymChink <vadim-vsnnschuk@ro.ru>
2022-05-10 13:08:39 +0300

    ts react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import {FC} from "react";
+
+import {Users} from "./Components";
+
+const App: FC = () => {
+
+    return (
+        <div>
+            <Users/>
+        </div>
+    );
+};
+
+export {
+    App
+};
